e527f30088d1d047d8b03d1612ce4cda3d4740c4
Mads Tinko Madsen <lyderslev1@gmail.com>
2022-11-09 23:15:07 +0100

    Removed duplicated code.

--- Symetric Encryption/Logic.cs
+++ Symetric Encryption/Logic.cs
@@ -9,6 +9,124 @@ namespace Symetric_Encryption
 {
     public class Logic
     {
+        public byte[] EncryptStringToBytes(string encryptionChoice, string plainText, byte[] Key, byte[] IV, string cipherMode)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            if (cipherMode == null || cipherMode.Length <= 0)
+                throw new ArgumentNullException("cipherMode");
+
+            byte[] encrypted;
+            // Create an AES object
+            // with the specified key and IV.
+            SymmetricAlgorithm mySymetricAlgorithm = null;
+
+
+            if (encryptionChoice == "TrippleDes")
+            {
+                mySymetricAlgorithm = TripleDES.Create();
+            }
+            else if (encryptionChoice == "AES")
+            {
+                mySymetricAlgorithm = Aes.Create();
+            }
+            mySymetricAlgorithm.Key = Key;
+            mySymetricAlgorithm.IV = IV;
+
+            if (cipherMode == "CBC")
+            {
+                mySymetricAlgorithm.Mode = CipherMode.CBC;
+            }
+            else if (cipherMode == "ECB")
+            {
+                mySymetricAlgorithm.Mode = CipherMode.ECB;
+            }
+            mySymetricAlgorithm.Padding = PaddingMode.PKCS7;
+
+            // Create an encryptor to perform the stream transform.
+            ICryptoTransform encryptor = mySymetricAlgorithm.CreateEncryptor(mySymetricAlgorithm.Key, mySymetricAlgorithm.IV);
+
+            // Create the streams used for encryption.
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        //Write all data to the stream.
+                        swEncrypt.Write(plainText);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        public string DecryptStringFromBytes(string encryptionChoice, byte[] cipherText, byte[] Key, byte[] IV, string cipherMode)
+        {
+            // Check arguments.
+            if (encryptionChoice == null || encryptionChoice.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            if (cipherMode == null || cipherMode.Length <= 0)
+                throw new ArgumentNullException("cipherMode");
+
+            string plaintext = null;
+
+            SymmetricAlgorithm mySymetricAlgorithm = null;
+
+            if (encryptionChoice == "TrippleDes")
+            {
+                mySymetricAlgorithm = TripleDES.Create();
+            }
+            else if (encryptionChoice == "AES")
+            {
+                mySymetricAlgorithm = Aes.Create();
+            }
+
+            mySymetricAlgorithm.Key = Key;
+            mySymetricAlgorithm.IV = IV;
+            if (cipherMode == "CBC")
+            {
+                mySymetricAlgorithm.Mode = CipherMode.CBC;
+            }
+            else if (cipherMode == "ECB")
+            {
+                mySymetricAlgorithm.Mode = CipherMode.ECB;
+            }
+            mySymetricAlgorithm.Padding = PaddingMode.PKCS7;
+
+            // Create a decryptor to perform the stream transform.
+            ICryptoTransform decryptor = mySymetricAlgorithm.CreateDecryptor(mySymetricAlgorithm.Key, mySymetricAlgorithm.IV);
+
+            // Create the streams used for decryption.
+            using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+
+                        // Read the decrypted bytes from the decrypting stream
+                        // and place them in a string.
+                        plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+            return plaintext;
+        }
+
         public byte[] CreateKeyWithUserInput(int length)
         {
             //Generate a cryptographic random number
